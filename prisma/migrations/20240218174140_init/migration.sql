a6ceac270363790b92f3db2d9644810ad9d5e8ea
CantilloLuis <cantilloluiseduardo52@gmail.com>
2024-02-18 12:54:34 -0500

    Modelo cars agregado al proyecto

---
+++ prisma/migrations/20240218174140_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Cars" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "marca" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "antiguedad" TEXT NOT NULL,
+    "modelo" TEXT NOT NULL
+);
